6a4b71c6793c074e092d2ace82448617ce417286
Gaurav <gauravdey625@gmail.com>
2022-02-15 00:16:03 +0530

    Removing Private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Login, Signin, AddDetails } from "./features/auth/index";
-import { PrivateRoute, Home, UserDetails, UpdateDetails } from './pages/index'
+import { Home, UserDetails, UpdateDetails } from './pages/index'
 function App() {
   return (
     <div className="App">
       <Routes>
-        <PrivateRoute path="/" element={<Home />} />
-        <PrivateRoute path="/user-details" element={<UserDetails />} />
-        <PrivateRoute path="/update-details" element={<UpdateDetails />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/user-details" element={<UserDetails />} />
+        <Route path="/update-details" element={<UpdateDetails />} />
         <Route path="/signup" element={<Signin />} />
         <Route path="/login" element={<Login />} />
         <Route path="/add-details" element={<AddDetails />} />
